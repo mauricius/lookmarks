5a9a2abf29b25e5f7abf9e2702a7e62cd82b3fdc
mauricius <maurizio.bonani@gmail.com>
2020-02-28 18:32:37 +0100

    Add small pagination view component

--- resources/presenters/PaginationPresenter.js
+++ resources/presenters/PaginationPresenter.js
@@ -15,6 +15,42 @@ class PaginationPresenter extends BasePresenter {
     return pagination.page == pagination.lastPage
   }
 
+  pageRange(pagination, delta = 2) {
+    if (pagination.length === 0) {
+      return [];
+    }
+
+    if (pagination.length === 1) {
+      return [1];
+    }
+
+    var current = parseInt(pagination.page, 10);
+    var last = pagination.lastPage;
+    var left = current - delta;
+    var right = current + delta + 1;
+    var range = [];
+    var pages = [];
+    var l;
+
+    for (var i = 1; i <= last; i++) {
+        if (i === 1 || i === last || (i >= left && i < right)) {
+            range.push(i);
+        }
+    }
+
+    range.forEach(function (i) {
+      if (l) {
+          if (i - l > 1) {
+              pages.push('...');
+          }
+      }
+      pages.push(i);
+      l = i;
+    });
+
+    return pages;
+  }
+
   appendQueryString(current_url, key, value) {
     const current = url.parse(current_url)
     const params = new URLSearchParams(current.search)
